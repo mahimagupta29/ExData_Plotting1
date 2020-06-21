e03e68c022504d51c8314096723199c3310f2fb7
mahimagupta29 <43781052+mahimagupta29@users.noreply.github.com>
2020-06-22 03:46:19 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -17,7 +17,8 @@ power_consumption_data$Global_active_power <- as.numeric(power_consumption_data$
 subsetdata <- subset(power_consumption_data, Date == "2007-02-01" | Date =="2007-02-02")
   
   ## plot histogram of global active power for those 2 days
-png("plot1.png", width=480, height=480)
+  # Plot must be saved as PNG files of 480x480 pixels
+png("plot1.png", width=480, height=480, units = "px")
 hist(subsetdata$Global_active_power, col="red", border="black", main ="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
   
 dev.off()
